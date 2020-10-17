119d1d530b3a121ff95a4e48b867f9868665a34e
Oscar Rene Duran Miranda <oscarrene380@gmail.com>
2020-10-17 10:01:38 -0600

    Initial commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$db = [
+    'host' => 'us-cdbr-east-02.cleardb.com',
+    'username' => 'b7f65caa8ceb9d',
+    'password' => 'e5835460',
+    'db' => 'heroku_b4a46a55e1f8b3a' //Cambiar al nombre de tu base de datos
+];
+
+?>
